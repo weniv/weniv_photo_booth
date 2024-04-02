db14db4612084cbd32dc400a678a83f658baf144
ujin16 <94890646+ujin16@users.noreply.github.com>
2024-04-02 14:21:21 +0900

    :wrench: Add: route 설정 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyle";
+import { DownloadResult, ScanQr, SelectFilter, SelectFrame, Shoot, Start } from "./pages";
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <GlobalStyles />
+            <Routes>
+                <Route path="/" element={<Start />} />
+                <Route path="/frame" element={<SelectFrame />} />
+                <Route path="/shoot" element={<Shoot />} />
+                <Route path="/filter" element={<SelectFilter />} />
+                <Route path="/result" element={<ScanQr />} />
+                <Route path="/download/:imgUrl/*" element={<DownloadResult />} />
+                <Route path="/*" element={<Start />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Start } from "./Start";
+export { default as SelectFrame } from "./SelectFrame";
+export { default as Shoot } from "./Shoot";
+export { default as SelectFilter } from "./SelectFilter";
+export { default as ScanQr } from "./ScanQr";
+export { default as DownloadResult } from "./DownloadResult.jsx";
