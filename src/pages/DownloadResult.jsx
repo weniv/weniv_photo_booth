cab4d54d665aeca2d693f2c2be31e36dc15de580
ujin16 <94890646+ujin16@users.noreply.github.com>
2024-04-11 08:18:58 +0900

    :sparkles: Feat: 다운로드 페이지

--- src/pages/DownloadResult.jsx
+++ src/pages/DownloadResult.jsx
@@ -1,3 +1,104 @@
+import styled from "styled-components";
+import Logo from "../assets/logo-mobile.svg";
+import { useLocation } from "react-router-dom";
+
 export default function DownloadResult() {
-    return <></>;
+    const location = useLocation();
+    const path = location.pathname.split("download/")[1];
+    const query = location.search;
+    const imageUrl = "https://firebasestorage.googleapis.com/v0/b/photo-booth-5ba8e.appspot.com/o/images" + path + query;
+    const videoUrl = "https://firebasestorage.googleapis.com/v0/b/photo-booth-5ba8e.appspot.com/o/videos" + path.split(".")[0] + ".mp4" + query;
+
+    const downloadImageFile = () => {
+        fetch(imageUrl, { method: "GET" })
+            .then((res) => {
+                return res.blob();
+            })
+            .then((blob) => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = "weniv4cut";
+                document.body.appendChild(a);
+                a.click();
+                setTimeout((_) => {
+                    window.URL.revokeObjectURL(url);
+                }, 60000);
+                a.remove();
+            })
+            .catch((err) => {
+                console.error("err: ", err);
+            });
+    };
+
+    const downloadVideoFile = () => {
+        fetch(videoUrl, { method: "GET" })
+            .then((res) => {
+                return res.blob();
+            })
+            .then((blob) => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = "weniv4cut.mp4";
+                document.body.appendChild(a);
+                a.click();
+                setTimeout((_) => {
+                    window.URL.revokeObjectURL(url);
+                }, 60000);
+                a.remove();
+            })
+            .catch((err) => {
+                console.error("err: ", err);
+            });
+    };
+
+    return (
+        <Wrap>
+            <FlexBox>
+                <h1>
+                    <img src={Logo} alt="jeju AI conference AI 네컷" />
+                </h1>
+                <Preview src={imageUrl} alt="" />
+                <ButtonWrap>
+                    <button onClick={downloadImageFile}>Image Download</button>
+                    <button onClick={downloadVideoFile}>Video Download</button>
+                </ButtonWrap>
+            </FlexBox>
+        </Wrap>
+    );
 }
+
+const Wrap = styled.main`
+    width: 100vs;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const FlexBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.87rem;
+`;
+
+const Preview = styled.img`
+    width: 80%;
+`;
+
+const ButtonWrap = styled.div`
+    width: 80%;
+    display: flex;
+    gap: 1.5rem;
+
+    button {
+        padding: 0.4375rem 1.5rem;
+        border-radius: 0.3125rem;
+        color: var(--conf-main, #160e26);
+        text-align: center;
+        font-family: DungGeunMo;
+        font-size: 1.25rem;
+    }
+`;
